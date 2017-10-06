d79222b47a05e89b22305f00630e8a0734e00231
ponychan <rzsckbmx@163.com>
2017-10-06 12:06:37 +0800

    3.12.cpp
    引用传递

--- ConsoleApplication3_12/ConsoleApplication312.cpp
+++ ConsoleApplication3_12/ConsoleApplication312.cpp
@@ -1,11 +1,23 @@
 // ConsoleApplication312.cpp: 定义控制台应用程序的入口点。
-//
+//引用传递交换两个整数
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+//交换子函数
+void swap(int &a, int &b) {
+	int t = a;
+	a = b;
+	b = t;
+}
 
 int main()
 {
-    return 0;
+	int x = 5, y = 10;
+	cout << "x=" << x << " y=" << y << endl;
+	swap(x, y);
+	cout << "x=" << x << " y=" << y << endl;
+	return 0;
 }
 
